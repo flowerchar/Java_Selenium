04fac8dc79aad13a68fe7771433eb3d7a44dd075
flowerchar <flowerchar2001@gmail.com>
2023-11-18 12:54:36 +0800

    feat: 23省赛web答案

---
+++ src/com/flowerchar/web/WEB_功能_再生资源系统答案 (1).java
@@ -0,0 +1,323 @@
+{
+	"name": "Renewable resources",
+	"checkpoints": [
+		{
+			"url": "121.43.149.85",
+			"el": "el1",
+			"from": null,
+			"locator": {
+				"method": "id",
+				"arg": "username"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el2",
+			"from": null,
+			"locator": {
+				"method": "id",
+				"arg": "password"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el3",
+			"from": null,
+			"locator": {
+				"method": "id",
+				"arg": "loginbutton"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el4",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[1]/div/div[1]/div[2]/div[1]/div/ul/div[2]/li/div"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el5",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[1]/div/div[1]/div[2]/div[1]/div/ul/div[2]/li/ul/div[1]/a/li"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el6",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div/div/div[2]/section/div[1]/div[1]/div[2]/span/button[1]/span"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el7",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[2]/div/div[2]/form/div[1]/div/div/input"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el8",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[2]/div/div[2]/form/div[2]/div/div/div[1]/i"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el9",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[3]/div/div[2]/section/main/div/div[2]/div/div[1]/div/div[3]/div/div/div[2]/label/span[1]/span"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el10",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[3]/div/div[3]/span/button[2]"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el11",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[2]/div/div[2]/form/div[3]/div/div/label[2]/span[2]"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el12",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[2]/div/div[3]/div/button[2]"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el13",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[1]/div/div[2]/section/div[1]/div[1]/div[1]/div/input"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el14",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[1]/div/div[2]/section/div[1]/div[1]/div[1]/span/button[1]"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el15",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[1]/div/div[2]/section/div[1]/div[2]/div[4]/div[2]/table/tbody/tr/td[5]/div/div/span/span/button"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el16",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[4]/div[1]/button[2]"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el17",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div/div/div[1]/div[2]/div[1]/div/ul/div[4]/li/div"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el18",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div/div/div[1]/div[2]/div[1]/div/ul/div[4]/li/ul/div[3]/a/li/span"
+			}
+		},
+        {
+			"url": "121.43.149.85",
+			"el": "el19",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div/div/div[2]/section/div[1]/div[1]/div/button/span"
+			}
+		},
+        {
+			"url": "121.43.149.85",
+			"el": "el20",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[1]/div/div[2]/section/div[1]/div[2]/div[3]/table/tbody/tr[1]/td[4]/div/span/span/button"
+			}
+		},
+		{
+			"url": "121.43.149.85",
+			"el": "el21",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[3]/div[1]/button[2]"
+			}
+		}
+	],
+	"gradepoints": [
+		{
+			"el": "el1",
+			"name": "op1",
+			"op": "sendKeys",
+			"arg": ".*"
+		},
+		{
+			"el": "el2",
+			"name": "op2",
+				"op": "sendKeys",
+			"arg": ".*"
+		},
+		{
+			"el": "el3",
+			"name": "op3",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el4",
+			"name": "op4",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el5",
+			"name": "op5",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el6",
+			"name": "op6",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el7",
+			"name": "op7",
+			"op": "sendKeys",
+			"arg": ".*"
+		},
+		{
+			"el": "el8",
+			"name": "op8",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el9",
+			"name": "op9",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el10",
+			"name": "op10",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el11",
+			"name": "op11",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el12",
+			"name": "op12",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el13",
+			"name": "op13",
+			"op": "sendKeys",
+			"arg": ".*"
+		},
+		{
+			"el": "el14",
+			"name": "op14",
+		"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el15",
+			"name": "op15",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el16",
+			"name": "op16",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el17",
+			"name": "op17",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el18",
+			"name": "op18",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el19",
+			"name": "op19",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el20",
+			"name": "op20",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el21",
+			"name": "op21",
+			"op": "click",
+			"arg": null
+		}
+		
+	]
+}
---
+++ src/com/flowerchar/web/WEB_功能_再生资源系统答案 (2).java
@@ -0,0 +1,413 @@
+{
+	"name": "ifeng",
+	"checkpoints": [
+		{
+			"url": "ifeng.com",
+			"el": "el1",
+			"from": null,
+			"locator": {
+				"method": "linkText",
+				"arg": "资讯"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el2",
+			"from": null,
+			"locator": {
+				"method": "linkText",
+				"arg": "新时代"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el3",
+			"from": null,
+			"locator": {
+				"method": "linkText",
+				"arg": "+ 更多新闻"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el4",
+			"from": null,
+			"locator": {
+				"method": "className",
+				"arg": "index_go_to_top_A0C-y"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el5",
+			"from": null,
+			"locator": {
+				"method": "linkText",
+				"arg": "凤凰资讯"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el6",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div[1]/div[3]/div[3]/div[2]/input"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el7",
+			"from": null,
+			"locator": {
+				"method": "className",
+				"arg": "index_btn_S-5T7"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el8",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div/div[2]/div[3]/div[2]/ul/li[1]/div/h2/a"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el9",
+			"from": null,
+			"locator": {
+				"method": "className",
+				"arg": "index_item_box_t--L6"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el10",
+			"from": null,
+			"locator": {
+				"method": "linkText",
+				"arg": "2023慕尼黑车展：大众全新Tiguan亮相，预计2024年上市"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el11",
+			"from": null,
+			"locator": {
+				"method": "className",
+				"arg": "text-2sXTFgZW"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el12",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div/div[3]/div[2]/div[1]/div[2]/div[5]/div[2]/div[1]/div/div[2]/span[1]/a[2]"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el13",
+			"from": null,
+			"locator": {
+				"method": "className",
+				"arg": "w-submitBtn"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el14",
+			"from": null,
+			"locator": {
+				"method": "className",
+				"arg": "w-commentArea"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el15",
+			"from": null,
+			"locator": {
+				"method": "className",
+				"arg": "icon-faceTrigArr"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el16",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div/div[3]/div[2]/div[1]/div[2]/div[5]/div[2]/div[1]/div/div[3]/div[1]/form/div[2]/div/div/div[2]/div/div[2]/ul/li[20]/img"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el17",
+			"from": null,
+			"locator": {
+				"method": "id",
+				"arg": "js_toTop"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el18",
+			"from": null,
+			"locator": {
+				"method": "linkText",
+				"arg": "凤凰网首页"
+			}
+		},
+        {
+			"url": "ifeng.com",
+			"el": "el19",
+			"from": null,
+			"locator": {
+				"method": "linkText",
+				"arg": "视频"
+			}
+		},
+        {
+			"url": "ifeng.com",
+			"el": "el20",
+			"from": null,
+			"locator": {
+				"method": "className",
+				"arg": "index_more_T32Gd"
+			}
+		},
+        {
+			"url": "ifeng.com",
+			"el": "el21",
+			"from": null,
+			"locator": {
+				"method": "linkText",
+				"arg": "美食"
+			}
+		},
+        {
+			"url": "ifeng.com",
+			"el": "el22",
+			"from": null,
+			"locator": {
+				"method": "xpath",
+				"arg": "/html/body/div/div[4]/div[1]/div[2]/a/div/img"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el23",
+			"from": null,
+			"locator": {
+				"method": "className",
+				"arg": "index_vSelect_1csYP"
+			}
+		},
+        {
+			"url": "ifeng.com",
+			"el": "el24",
+			"from": null,
+			"locator": {
+				"method": "linkText",
+				"arg": "凤凰首页"
+			}
+		},
+        {
+			"url": "ifeng.com",
+			"el": "el25",
+			"from": null,
+			"locator": {
+				"method": "className",
+				"arg": "index_login_name_2x_UU-Kq"
+			}
+		},
+        {
+			"url": "ifeng.com",
+			"el": "el26",
+			"from": null,
+			"locator": {
+				"method": "linkText",
+				"arg": "进入个人中心"
+			}
+		},
+		{
+			"url": "ifeng.com",
+			"el": "el27",
+			"from": null,
+			"locator": {
+				"method": "linkText",
+				"arg": "注销"
+			}
+		}
+	],
+	"gradepoints": [
+		{
+			"el": "el1",
+			"name": "op1",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el2",
+			"name": "op2",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el3",
+			"name": "op3",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el4",
+			"name": "op4",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el5",
+			"name": "op5",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el6",
+			"name": "op6",
+			"op": "sendKeys",
+			"arg": ".*"
+		},
+		{
+			"el": "el7",
+			"name": "op7",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el8",
+			"name": "op8",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el9",
+			"name": "op9",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el10",
+			"name": "op10",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el11",
+			"name": "op11",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el12",
+			"name": "op12",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el13",
+			"name": "op13",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el14",
+			"name": "op14",
+			"op": "sendKeys",
+			"arg": ".*"
+		},
+		{
+			"el": "el15",
+			"name": "op15",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el16",
+			"name": "op16",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el17",
+			"name": "op17",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el18",
+			"name": "op18",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el19",
+			"name": "op19",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el20",
+			"name": "op20",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el21",
+			"name": "op21",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el22",
+			"name": "op22",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el23",
+			"name": "op23",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el24",
+			"name": "op24",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el25",
+			"name": "op25",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el26",
+			"name": "op26",
+			"op": "click",
+			"arg": null
+		},
+		{
+			"el": "el27",
+			"name": "op27",
+			"op": "click",
+			"arg": null
+		}
+		
+	]
+}
